7a9152bb18c0313449e448bc8706ab9b2a96ab4c
cheungbh <534660436@qq.com>
2019-10-15 15:27:22 +0800

    Image detection to a function

--- detection.py
+++ detection.py
@@ -4,10 +4,10 @@ import os
 import numpy as np
 
 water_top = 105
-write = True
+write = False
 
-img_num = 1
-detect_img = "suspect/{}.jpg".format(img_num)
+img_num = 233
+detect_img = "img/multiple.jpg"
 origin_frame = "img/origin.jpg"
 standard_frame = cv2.imread(origin_frame)
 
@@ -19,8 +19,11 @@ cv2.imshow("diff", diff)
 cut_diff = Utils.cut_image(diff, top=water_top)
 cv2.imshow("cut", cut_diff)
 
+blur = cv2.blur(cut_diff, (7, 7))
+cv2.imshow("blur", blur)
+
 enhance_kernel = np.array([[0, -1, 0], [0, 5, 0], [0, -1, 0]])
-imageEnhance = cv2.filter2D(cut_diff, -1, enhance_kernel)
+imageEnhance = cv2.filter2D(blur, -1, enhance_kernel)
 cv2.imshow("laplancian", imageEnhance)
 
 hsv = cv2.cvtColor(imageEnhance, cv2.COLOR_BGR2HSV)
@@ -31,11 +34,11 @@ upper = np.array([180, 255, 255])
 thresh = cv2.inRange(hsv, lowerb=lower, upperb=upper)
 cv2.imshow("thresh", thresh)
 
-open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
-opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, open_kernel)
-cv2.imshow("opening", opening)
+dilate_kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+dilation = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, dilate_kernel)
+cv2.imshow("dilation", dilation)
 
-contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 stored = [idx for idx in range(len(contours)) if len(contours[idx]) > 80]
 real_con = [contours[i] for i in stored]
 
@@ -48,9 +51,10 @@ if write:
     os.makedirs(out_folder, exist_ok=True)
     cv2.imwrite("{}/diff.jpg".format(out_folder), diff)
     cv2.imwrite("{}/cut_diff.jpg".format(out_folder), cut_diff)
+    cv2.imwrite("{}/blur.jpg".format(out_folder), blur)
     cv2.imwrite("{}/enhance.jpg".format(out_folder), imageEnhance)
     cv2.imwrite("{}/thresh.jpg".format(out_folder), thresh)
-    cv2.imwrite("{}/opening.jpg".format(out_folder), opening)
+    cv2.imwrite("{}/opening.jpg".format(out_folder), dilation)
     cv2.imwrite("{}/result.jpg".format(out_folder), frame)
 
 cv2.imshow("detection", frame)
--- video_det.py
+++ video_det.py
@@ -2,9 +2,10 @@ import os
 import cv2
 import numpy as np
 from utils.utils import Utils
+import sys
 
-video_path = "img/drown1.mp4"
-cap = cv2.VideoCapture(video_path)
+video_path = sys.argv[1]
+cap = cv2.VideoCapture(os.path.join("img", video_path))
 water_top = 105
 standard_frame = cv2.imread("img/origin.jpg")
 os.makedirs("frame", exist_ok=True)
@@ -21,17 +22,25 @@ while True:
         imageEnhance = cv2.filter2D(blur, -1, enhance_kernel)
         hsv = cv2.cvtColor(imageEnhance, cv2.COLOR_BGR2HSV)
         thresh = cv2.inRange(hsv, lowerb=np.array([0, 0, 46]), upperb=np.array([180, 255, 255]))
-        dilate_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        dilate_kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
         dilation = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, dilate_kernel)
         contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         stored = [idx for idx in range(len(contours)) if len(contours[idx]) > 80]
+        rects = []
         real_con = [contours[i] for i in stored]
         for c in real_con:
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y + water_top), (x + w, y + water_top + h), (0, 255, 0), 2)
+            rects = [x, y, x + w, y + water_top + h]
+            human = Utils.cut_image(frame, top=x, bottom=x+w, left=y, )
+            cv2.imwrite()
         cv2.imshow("detection", frame)
-        cv2.imwrite(os.path.join("frame", "{}.jpg".format(cnt)), frame)
-        cv2.waitKey(1)
+        #cv2.imwrite(os.path.join("frame", "{}.jpg".format(cnt)), frame)
+        if "multiple" in video_path:
+            time = 2
+        else:
+            time = 30
+        cv2.waitKey(time)
         print("Frame: {}".format(cnt))
         cnt += 1
     else:
