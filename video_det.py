b3772c31f7c510df1671e8046c4a9d8378afa166
cheungbh <534660436@qq.com>
2020-01-06 22:14:45 +0800

    modify the structure

--- video_det.py
+++ video_det.py
@@ -1,44 +1,73 @@
-import os
 import cv2
-import numpy as np
-from utils.utils import Utils
+import os
 from config import config
+import numpy as np
 
-main_folder = "img/drown1"
-video_path = os.path.join(main_folder, "drown.mp4")
-cap = cv2.VideoCapture(video_path)
-water_top = config.water_top
-standard_frame = cv2.imread(os.path.join(main_folder, "origin.jpg"))
-standard_frame = cv2.resize(standard_frame, config.frame_shape)
-os.makedirs(os.path.join(main_folder, "result"), exist_ok=True)
-os.makedirs(os.path.join(main_folder, "origin_frame"), exist_ok=True)
 cnt = 0
+save_ls = [175, 333, 556, 888, 1000, 1080]
+video_num = 54
+img_folder = str(video_num)
+
+cap = cv2.VideoCapture(os.path.join('Video/Selected', '{}.mp4'.format(img_folder)))
+fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=200, detectShadows=False)
+show = False
+write = False
 
 while True:
-    ret, origin_frame = cap.read()
-    if ret:
-        frame = cv2.resize(origin_frame, (standard_frame.shape[1], standard_frame.shape[0]))
-        #cv2.imwrite(os.path.join(main_folder, "origin_frame", "{}.jpg".format(cnt)), frame)
-        diff = cv2.absdiff(frame, standard_frame)
-        cut_diff = Utils.cut_image(diff, top=water_top)
-        blur = cv2.blur(cut_diff, config.blur_kernel)
-        imageEnhance = cv2.filter2D(blur, -1, config.enhance_kernel)
-        hsv = cv2.cvtColor(imageEnhance, cv2.COLOR_BGR2HSV)
-        thresh = cv2.inRange(hsv, lowerb=config.hsv_lower, upperb=config.hsv_upper)
-        dilate_kernel = cv2.getStructuringElement(config.dilation_method[0], config.dilation_kernel)
-        dilation = cv2.morphologyEx(thresh, config.dilation_method[1], dilate_kernel)
-        contours, hierarchy = cv2.findContours(dilation, config.contour_method[0], config.contour_method[1])
-        stored = [idx for idx in range(len(contours)) if len(contours[idx]) > config.real_con_len]
-        rects = []
-        real_con = [contours[i] for i in stored]
-        for c in real_con:
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y + water_top), (x + w, y + water_top + h), (0, 255, 0), 2)
-            rects = [x, y, x + w, y + water_top + h]
-        cv2.imshow("detection", frame)
-        #cv2.imwrite(os.path.join(main_folder, "result", "{}.jpg".format(cnt)), frame)
-        cv2.waitKey(2)
-        print("Frame: {}".format(cnt))
-        cnt += 1
-    else:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (540, 360))
+    cv2.imshow('input', frame)
+    fgmask = fgbg.apply(frame)
+    background = fgbg.getBackgroundImage()
+    diff = cv2.absdiff(frame, background)
+    point = diff
+
+    gray = cv2.cvtColor(point, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(src=gray, blockSize=9, ksize=27, k=0.04)
+    a = dst > 0.01 * dst.max()
+    point[a] = [0, 0, 255]
+    cv2.imshow("point", point)
+
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.blur(diff, config.blur_kernel)
+
+    imageEnhance = cv2.filter2D(blur, -1, config.enhance_kernel)
+
+    hsv = cv2.cvtColor(imageEnhance, cv2.COLOR_BGR2HSV)
+
+    thresh = cv2.inRange(hsv, lowerb=config.hsv_lower_black, upperb=config.hsv_upper_black)
+
+    dilate_kernel = cv2.getStructuringElement(config.dilation_method[0], config.dilation_kernel)
+    dilation = cv2.morphologyEx(thresh, config.dilation_method[1], dilate_kernel)
+
+    if show:
+        cv2.imshow('input', frame)
+        cv2.moveWindow("input", 0, 0)
+        cv2.imshow('background', background)
+        cv2.moveWindow("background", 0, 450)
+        cv2.imshow('mask', diff)
+        cv2.moveWindow("mask", 540, 0)
+        cv2.imshow('gray', gray)
+        cv2.moveWindow("gray", 1080, 450)
+        cv2.imshow("enhance", imageEnhance)
+        cv2.moveWindow("enhance", 540, 450)
+        cv2.imshow("threshold", thresh)
+        cv2.moveWindow("threshold", 1080, 0)
+
+    if write:
+        if cnt in save_ls:
+            des_path = os.path.join("frame/{}/{}".format(img_folder, cnt))
+            os.makedirs(des_path, exist_ok=True)
+            cv2.imwrite(os.path.join(des_path, "mask.jpg"), diff)
+            cv2.imwrite(os.path.join(des_path, "enhance.jpg"), imageEnhance)
+            cv2.imwrite(os.path.join(des_path, "gray.jpg"), gray)
+
+    print("Finish processing frame {}".format(cnt))
+    cnt += 1
+    k = cv2.waitKey(10)&0xff
+    if k == 27:
         break
+cap.release()
+cv2.destroyAllWindows()
---
+++ video_det_pre.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import numpy as np
+from utils.utils import Utils
+from config import config
+
+main_folder = "img/drown1"
+video_path = os.path.join(main_folder, "drown.mp4")
+cap = cv2.VideoCapture(video_path)
+water_top = config.water_top
+standard_frame = cv2.imread(os.path.join(main_folder, "origin.jpg"))
+standard_frame = cv2.resize(standard_frame, config.frame_shape)
+os.makedirs(os.path.join(main_folder, "result"), exist_ok=True)
+os.makedirs(os.path.join(main_folder, "origin_frame"), exist_ok=True)
+cnt = 0
+
+while True:
+    ret, origin_frame = cap.read()
+    if ret:
+        frame = cv2.resize(origin_frame, (standard_frame.shape[1], standard_frame.shape[0]))
+        #cv2.imwrite(os.path.join(main_folder, "origin_frame", "{}.jpg".format(cnt)), frame)
+        diff = cv2.absdiff(frame, standard_frame)
+        cut_diff = Utils.cut_image(diff, top=water_top)
+        blur = cv2.blur(cut_diff, config.blur_kernel)
+        imageEnhance = cv2.filter2D(blur, -1, config.enhance_kernel)
+        hsv = cv2.cvtColor(imageEnhance, cv2.COLOR_BGR2HSV)
+        thresh = cv2.inRange(hsv, lowerb=config.hsv_lower_black, upperb=config.hsv_upper_black)
+        dilate_kernel = cv2.getStructuringElement(config.dilation_method[0], config.dilation_kernel)
+        dilation = cv2.morphologyEx(thresh, config.dilation_method[1], dilate_kernel)
+        contours, hierarchy = cv2.findContours(dilation, config.contour_method[0], config.contour_method[1])
+        stored = [idx for idx in range(len(contours)) if len(contours[idx]) > config.real_con_len]
+        rects = []
+        real_con = [contours[i] for i in stored]
+        for c in real_con:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y + water_top), (x + w, y + water_top + h), (0, 255, 0), 2)
+            rects = [x, y, x + w, y + water_top + h]
+        cv2.imshow("detection", frame)
+        #cv2.imwrite(os.path.join(main_folder, "result", "{}.jpg".format(cnt)), frame)
+        cv2.waitKey(2)
+        print("Frame: {}".format(cnt))
+        cnt += 1
+    else:
+        break
